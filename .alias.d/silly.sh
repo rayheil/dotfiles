39b8b3a5913dede6eb524d6407965f56c70d3772
Almond Heil <almond@almendra.dev>
2023-08-12 06:48:27 -0600

    alias: add betterdiscord oneline updater

--- .alias.d/silly.sh
+++ .alias.d/silly.sh
@@ -3,3 +3,7 @@
 
 alias sl="sl -Fl"
 alias fuckbitches="speak-ng 'get money'"
+
+# oneliner to update / install betterdiscord at the flatpak install location
+# https://github.com/BetterDiscord/Installer/issues/264#issuecomment-1216171370
+alias betterdiscord-update="DISC_CONFIG=\"\$HOME/.var/app/com.discordapp.Discord/config/discord\" && BD_ASAR=betterdiscord.asar && wget --timestamping -P \"\${DISC_CONFIG}/../BetterDiscord/data\" -- \"https://github.com/BetterDiscord/BetterDiscord/releases/latest/download/\${BD_ASAR}\" && DISC_VER=\"\$(ls --sort=time --time=creation \"\${DISC_CONFIG}\" | grep -E -m 1 '^[0-9]+\\.[0-9]+\\.[0-9]+\$')\" && echo -e \"require('../../../../BetterDiscord/data/\${BD_ASAR}');\\nmodule.exports = require('./core.asar');\" | tee \"\${DISC_CONFIG}/\${DISC_VER}/modules/discord_desktop_core/index.js\" && echo -e \"\\nBetterDiscord installed. Restart Discord if currently running.\" || echo -e \"\\nInstallation failed.\""

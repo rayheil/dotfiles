b99f75ad2d430c74c4b4ab4b4b333ce32cf12c33
almond heil <almond@almendra.dev>
2024-01-09 23:08:26 -0700

    Remove flatpak aliases I don't use from terminal

--- .alias.d/flatpak.sh
+++ .alias.d/flatpak.sh
@@ -1,16 +1,8 @@
 # flatpak.sh
-# flatpak run aliases
+# flatpak run aliases for flatpaks I want to run from terminal
 
 # sort with !sort in visual line mode
-alias audacity="flatpak run org.audacityteam.Audacity"
 alias chrome="flatpak run com.google.Chrome"
-alias discord="flatpak run con.discordapp.Discord"
 alias firefox="flatpak run org.mozilla.firefox"
-alias flatseal="com.github.tchx84.Flatseal"
-alias minecraft="flatpak run com.mojang.Minecraft"
 alias nextcloud="flatpak run com.nextcloud.desktopclient.nextcloud"
-alias prismlauncher="flatpak run org.prismlauncher.PrismLauncher"
-alias spotify="flatpak run com.spotify.Client"
-alias thunderbird="flatpak run org.mozilla.Thunderbird"
 alias tor="flatpak run com.github.micahflee.torbrowser-launcher"
-alias zotero="flatpak run org.zotero.Zotero"
